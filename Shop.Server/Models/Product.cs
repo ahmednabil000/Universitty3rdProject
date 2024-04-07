9ffa27057b334a92ddcc268e1f79f848f5a6634f
Ahmed nabil <ahmednabil3400@gmail.com>
2024-04-08 01:11:04 +0200

    Scaffoldin the database into the application

---
+++ Shop.Server/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shop.Server.Models;
+
+public partial class Product
+{
+    public int PId { get; set; }
+
+    public string PName { get; set; } = null!;
+
+    public int PPrice { get; set; }
+
+    public string PDesc { get; set; } = null!;
+
+    public virtual ICollection<Bill> Bills { get; set; } = new List<Bill>();
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
